e9b1a9ed581327e17608b7abb0f0e36e7e39ed8a
黄永东 <luckeast@163.com>
2020-12-25 10:15:14 +0800

    update require.js.

--- require.js
+++ require.js
@@ -4,12 +4,18 @@
 		basePath = basePath || "/";
 
 		//需要模块
-		var require = function(paths, cb){
+		var require = function (p, cb) {
 			var resultArr = [], //得到的模块Exports的列表
 				asyncArr = [], //以下程序用到的异步处理的列表
 				async = cb != null; //是否异步处理
 
-			paths.forEach(function (path, pathIndex) {
+			if (async) {
+				if (typeof (p) == "string") throw "The asynchronous module uses multiple string array parameters";
+			} else {
+				if (typeof (p) != "string") throw "The synchronization module uses a single string parameter";
+            }
+
+			p.forEach(function (path, pathIndex) {
 				var fullPath = basePath + "/" + path + ".js";
 				fullPath = fullPath.replace("//", "/");
 
@@ -49,7 +55,7 @@
 					return cb.apply(win, resultArr);
 				})
 			} else {
-				if (paths.length <= 1) return resultArr[0]; else return resultArr;
+				if (p.length <= 1) return resultArr[0]; else return resultArr;
             }
 			
 		};
@@ -73,10 +79,10 @@
 
 		//判断调用参数数量
 		if (arguments.length == 1) {
-			module.require = []; //模块内部需要的模块名称的列表
+			module.requires = []; //模块内部需要的模块名称的列表
 			module.define = arguments[0]; //模块Exports的定义脚本
 		} else if (arguments.length == 2) {
-			module.require = arguments[0]; //模块内部需要的模块名称的列表
+			module.requires = arguments[0]; //模块内部需要的模块名称的列表
 			module.define = arguments[1]; //模块Exports的定义脚本
         }
 
@@ -90,38 +96,45 @@
 	//用于处理模块的服务
 	var moduleService = {
 		//处理模块依赖，然后执行模块定义，最后返回模块Exports
-		require_define_exports: function (module, async, asyncArr) {
+		require_define_exports: function (win2, require, module, async, asyncArr) {
 			//判断模块是否未完成定义
 			if (!module.defined) {
 				//如果没有传递回调函数，则用同步编码方式处理。如果有回调函数，则用异步编码方式处理。
 				if (async) { //有回调函数，将使用异步编码方式处理。
 					asyncArr.push(new Promise(function (resolve) {
 						//处理依赖模块需要
-						win.require(module.require, function () {
-							moduleService.define(module);
-							resolve(); //定义完成再通知回调执行
+						win.require(module.requires, function () {
+							//处理依赖模块需要
+							for (var i = 0; i < module.requires.length; i++) win.require(module.requires[i]);
+
+							//returnExports用于实现AMD的return exports
+							var returnExports = module.define.apply(win2, arguments);
+							//不允许同时存在module.exports和return exports两种实现方式，只能选择一种实现方式。即不能同时实现amd和cmd。
+							if (returnExports != null) throw "Asynchronous module export with return exports";
+							//从return exports或者module.exports得到模块的exports
+							module.exports = returnExports;
+							//设置模块为已定义完成状态
+							module.defined = true;
+							resolve(); 
 						});
 					}));
 				} else { //没有回调函数，将使用同步编码方式处理。
 					//处理依赖模块需要
-					win.require(module.require);
-					moduleService.define(module);
+					for (var i = 0; i < module.requires.length; i++) win.require(module.requires[i]);
+
+					//paramModule用于实现CMD的module.exports
+					var paramModule = {};
+					//returnExports用于实现AMD的return exports
+					var returnExports = module.define.call(win2, require, paramModule);
+					//不允许同时存在module.exports和return exports两种实现方式，只能选择一种实现方式。即不能同时实现amd和cmd。
+					if (returnExports != null) throw "Synchronization module export by module.exports";
+					//从return exports或者module.exports得到模块的exports
+					module.exports = paramModule.exports;
+					//设置模块为已定义完成状态
+					module.defined = true;
 				}
 			}
 			return module.exports;
-		},
-		//执行模块定义
-		define: function (module) {
-			//paramModule用于实现CMD的module.exports
-			var paramModule = {};
-			//returnExports用于实现AMD的return exports
-			var returnExports = module.define(win, paramModule);
-			//不允许同时存在module.exports和return exports两种实现方式，只能选择一种实现方式。
-			if (returnExports != null && module.exports != null) throw "module.exports cannot be used with return exports";
-			//从return exports或者module.exports得到模块的exports
-			module.exports = returnExports || paramModule.exports;
-			//设置模块为已定义完成状态
-			module.defined = true;
-        }
+		}
 	};
 })(window);
